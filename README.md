4bab73c493f7b0d91ab67bf01ca417a6bdc1dd05
Scriptbash <98601298+Scriptbash@users.noreply.github.com>
2024-09-19 18:28:03 -0400

    First push

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# DaymetDownloaderForRaven
-Downloads and process Daymet data for use in the Raven hydrological modelling framework
+# Daymet downloader for Raven
+Download and process Daymet data for use in the Raven hydrological modelling framework.
+
+## Features
+ - Download Daymet data for precipitation and minimum/maximum temperature using only a polygon shapefile.
+ - Fix NaN values and insert missing dates
+ - Merge the downloaded files
+ - (Planned) Generate grid weights
---
+++ main.py
@@ -0,0 +1,274 @@
+import os
+#import sys
+import warnings
+import urllib.request
+import numpy as np
+import xarray as xr
+import pandas as pd
+import geopandas as gpd
+from datetime import datetime, timedelta, timezone
+
+warnings.filterwarnings("ignore", category=RuntimeWarning, module='xarray')
+
+
+options = { 
+            'polygon_shp':r'/home/francis/Downloads/canada_new_watershed(1)/haultain/watershed.shp',
+            'start': datetime.strptime('2000-01-01', "%Y-%m-%d"),
+            'end': datetime.strptime('2020-12-31', "%Y-%m-%d"),
+            'variables':['tmin', 'tmax', 'prcp'],
+            'nan_fix': True,
+            'merge': True,
+            'output_folder': r'./output_haultain',
+            'timeout':1800,
+           }
+
+# def get_search_criteria(options):
+#     output_folder = options['polygon_shp']
+#     start_date = dlg.date_daymet_start_date.date().toPyDate()
+#     end_date = dlg.date_daymet_end_date.date().toPyDate()
+#     selected_variables = dlg.list_daymet_variables.selectedItems()
+#     variables = ""
+#     if selected_variables:
+#         for item in selected_variables:
+#             variables += item.text() + ' '
+#     if start_date and end_date and variables:
+#         dlg.lbl_daymet_error.clear()
+#         bbox = self.define_area(dlg)
+#         self.get_data(bbox, start_date, end_date, variables, output_folder, dlg)
+#     else:
+#         print("At least one variable must be selected.")
+#         sys.exit()
+
+def define_area(options):
+    input_polygon = options['polygon_shp']
+    gdf = gpd.read_file(input_polygon)
+    crs = gdf.crs
+    if crs.to_string() != 'EPSG:4326':
+        print('CRS mismatch... input layer CRS is ' + crs.to_string())
+        print('Reprojecting layer to EPSG:4326...')
+        gdf = gdf.to_crs('EPSG:4326')
+        print('Reprojection done.')
+
+    print("Extracting bounding box...")
+    bbox_list = []
+
+    for i, row in gdf.iterrows():
+        bbox = row.geometry.bounds  # Returns (xmin, ymin, xmax, ymax)
+        bbox = [str(round(bbox[0], 2)), str(round(bbox[1], 2)), 
+                str(round(bbox[2], 2)), str(round(bbox[3], 2))]
+        bbox_list.append(bbox)
+
+    print('Bounding box extraction done.')
+    return bbox_list
+
+def get_data(options, bbox):
+    timeout = options['timeout']
+    region = "na"
+
+    if not os.path.exists(options['output_folder']):
+        os.makedirs(options['output_folder'])
+        print('Output folder did not exist. One was created.')
+
+    print('Initializing...')
+    for i in range(len(bbox)):
+        north = bbox[i][3]
+        west = bbox[i][0]
+        east = bbox[i][2]
+        south = bbox[i][1]
+
+        for variable in options['variables']:
+            for year in range(int(options['start'].year), int(options['end'].year)+1):
+                url = "https://thredds.daac.ornl.gov/thredds/ncss/grid/ornldaac/2129/daymet_v4_daily_" + region + "_" \
+                        + variable + '_' + str(year) + ".nc?var=lat&var=lon&var=" + variable + '&north=' + north + \
+                        "&west=" + west + "&east=" + east + "&south=" + south + \
+                        "&disableProjSubset=on&horizStride=1&time_start=" + \
+                        str(options['start'].date()) + "T12:00:00Z&time_end=" + str(options['end'].date()) + "T12:00:00Z&timeStride=1&accept=netcdf"
+                req = urllib.request.Request(url)
+                #print(url)
+                response = urllib.request.urlopen(req, timeout=timeout)
+                totalsize = int(response.info()['Content-Length'])
+                currentsize = 0
+                old_percentage = 0
+                chunk = 4096
+
+                filename = str(year) + variable + '.nc'
+                output_file = options['output_folder'] + "/" + filename
+                print("Variable " + str(options['variables'].index(variable) + 1) + "/" + str(len(options['variables'])) +
+                                                " - Downloading " + filename)
+                with open(output_file, 'wb') as file:
+                    while 1:
+                        data = response.read(chunk)
+                        if not data:
+                            break
+                        file.write(data)
+                        currentsize += chunk
+                        if totalsize > 0:
+                            download_percentage = (currentsize / totalsize) * 100
+                            if int(download_percentage) > old_percentage:
+                                print(f"\rDownload progress: {int(download_percentage)}%", end='', flush=True)
+                                old_percentage = int(download_percentage)
+                print()
+                if options['nan_fix']:
+                    missing_dates = check_missing_dates(output_file)
+                    fix_missing_values(output_file, missing_dates, variable)
+                else:
+                    pass
+            if options['merge']:
+                print('Merging files...')
+                merge_netcdf(options['output_folder'], variable)
+                print('Merge complete.')
+        print("Download complete!")
+    
+
+def merge_netcdf(file_path, variable):
+    try:
+        ds = xr.open_mfdataset(file_path + '/*' + variable + '.nc', parallel=False)
+        # Round down the time values to the nearest integer (remove the 0.5)
+        ds['time'] = ds['time'].dt.floor('D')
+        # Remove the time dimension from the lat and lon variables
+        lat_without_time = ds['lat'].isel(time=0)
+        lon_without_time = ds['lon'].isel(time=0)
+        ds_modified = xr.Dataset({
+            'lat': lat_without_time,
+            'lon': lon_without_time,
+            variable: ds[variable]
+        })
+        #ds_modified['time'] = ds_modified['time'].astype(int)
+        ds_modified.to_netcdf(file_path + '/' + variable + '_merged.nc')
+        ds.close()
+        ds_modified.close()
+    except Exception as e:
+        print('The merging attempt failed. Manual processing will be required.')
+        print(e)
+        return
+    
+
+def check_missing_dates(ncfile):
+    ds = xr.open_dataset(ncfile)
+    time_data = ds['time'].values
+    start_year = int(pd.Timestamp(time_data.min()).strftime('%Y'))
+    end_year = int(pd.Timestamp(time_data.max()).strftime('%Y'))
+    datetime_list = [datetime.fromtimestamp(ts.astype('O') / 1e9, timezone.utc).replace(tzinfo=None) for ts in time_data]
+    
+
+    start_date = datetime(start_year, 1, 1, 12, 0)
+    end_date = datetime(end_year, 12, 31, 12, 0)
+
+    all_dates = [start_date + timedelta(days=x) for x in range((end_date - start_date).days + 1)]
+
+    missing_dates = [date for date in all_dates if date not in datetime_list]
+    ds.close()
+    return missing_dates
+
+
+def fix_missing_values(ncfile, missing_dates, variable):
+    ds = xr.open_dataset(ncfile)
+
+    if variable == 'prcp':
+        if missing_dates:
+            # Create an empty DataArray with NaN values for the missing dates
+            missing_data = xr.full_like(ds.isel(time=0), fill_value=float(0.0))
+            missing_data['time'] = missing_dates
+            # Concatenate the missing data with the ds along the 'time' dimension
+            updated_data = xr.concat([ds, missing_data], dim='time')
+            updated_data = updated_data.fillna(float(0.0))
+            #updated_data.to_netcdf(ncfile)
+        else:
+            updated_data = ds.fillna(float(0.0))
+            # updated_data.to_netcdf('./result/test.nc')
+    else:
+        if ds.isnull().any():
+            print('Found NaN values. Attempting to fix...')
+            try:
+                radius = 1
+                # Find indices of missing values
+                missing_indices = np.argwhere(np.isnan(ds[variable].values))
+
+                for idx in missing_indices:
+                    time_idx, lat_idx, lon_idx = idx
+
+                    # Find neighboring values within the radius
+                    y_slice = slice(max(lat_idx - radius, 0), min(lat_idx + radius + 1, len(ds['y'])))
+                    x_slice = slice(max(lon_idx - radius, 0), min(lon_idx + radius + 1, len(ds['x'])))
+                    neighbor_values = ds[variable].isel(time=time_idx, y=y_slice, x=x_slice)
+
+                    # Exclude NaN values and compute the mean
+                    neighbor_mean = np.nanmean(neighbor_values)
+
+                    # Fill missing values with the mean of neighboring values
+                    ds[variable].values[time_idx, lat_idx, lon_idx] = neighbor_mean
+                print('Done.')
+            except Exception as e:
+                print('Unable to fix the NaN values.')
+                print(e)
+     
+        if missing_dates:
+            missing_data = xr.full_like(ds.isel(time=0), fill_value=np.nan, dtype=float)
+            missing_data['time'] = missing_dates
+            # Concatenate the missing data with the ds along the 'time' dimension
+            updated_data = xr.concat([ds, missing_data], dim='time')
+            for date in missing_dates:
+                try:
+                    # Attempt to extract values for the day before and day after
+                    # Find nearest available dates (day before and day after)
+                    before_date = (pd.to_datetime(date) - pd.Timedelta(days=1)).strftime('%Y-%m-%d')
+                    after_date = (pd.to_datetime(date) + pd.Timedelta(days=1)).strftime('%Y-%m-%d')
+
+                    if before_date in [str(ts)[:10] for ts in updated_data.time.values] and after_date in [str(ts)[:10]
+                                                                                                           for ts in
+                                                                                                           updated_data.time.values]:
+                        before_values = updated_data[variable].sel(time=before_date, method='nearest').values
+                        after_values = updated_data[variable].sel(time=after_date, method='nearest').values
+                        if not np.any(np.isnan(before_values)) and not np.any(np.isnan(after_values)):
+                            # Calculate the average
+                            average_value = (before_values + after_values) / 2.0
+                            # Assign the average value to the missing date
+                            updated_data[variable].loc[dict(time=date)] = average_value
+                            print('Using average of the day before and the day after for interpolation.')
+                            continue
+
+                    before_date = (pd.to_datetime(date) - pd.Timedelta(days=1)).strftime('%Y-%m-%d')
+                    after_date = (pd.to_datetime(date) - pd.Timedelta(days=2)).strftime('%Y-%m-%d')
+                    if before_date in [str(ts)[:10] for ts in updated_data.time.values] and after_date in [str(ts)[:10]
+                                                                                                           for ts in
+                                                                                                           updated_data.time.values]:
+                        before_values = updated_data[variable].sel(time=before_date, method='nearest').values
+                        after_values = updated_data[variable].sel(time=after_date, method='nearest').values
+                        if not np.any(np.isnan(before_values)) and not np.any(np.isnan(after_values)):
+                            # Calculate the average
+                            average_value = (before_values + after_values) / 2.0
+                            # Assign the average value to the missing date
+                            updated_data[variable].loc[dict(time=date)] = average_value
+                            print('Using average of the two days before for interpolation.')
+                            continue
+
+                    before_date = (pd.to_datetime(date) + pd.Timedelta(days=1)).strftime('%Y-%m-%d')
+                    after_date = (pd.to_datetime(date) + pd.Timedelta(days=2)).strftime('%Y-%m-%d')
+                    if before_date in [str(ts)[:10] for ts in updated_data.time.values] and after_date in [str(ts)[:10]
+                                                                                                           for ts in
+                                                                                                           updated_data.time.values]:
+                        before_values = updated_data[variable].sel(time=before_date, method='nearest').values
+                        after_values = updated_data[variable].sel(time=after_date, method='nearest').values
+                        if not np.any(np.isnan(before_values)) and not np.any(np.isnan(after_values)):
+                            # Calculate the average
+                            average_value = (before_values + after_values) / 2.0
+                            # Assign the average value to the missing date
+                            updated_data[variable].loc[dict(time=date)] = average_value
+                            print('Using average of the two days after for interpolation.')
+                            continue
+                    print('Missing values are present. Could not interpolate.')
+
+                except KeyError:
+                    print(f"No data available for {date}. Skipping.")
+
+        else:
+            updated_data = ds
+
+    updated_data.to_netcdf(ncfile)
+    updated_data.close()
+
+    ds.close()
+
+
+bbox = define_area(options)
+get_data(options, bbox)
